e55a9dd5d4a42896ed2c40ddfe9808b80e594140
Reshab Kumar <rkmodi12051998@gmail.com>
2026-01-03 15:43:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,77 @@
+// var a = 9;
+// // var a ;
+// // a = 16;
+// // var a = 18;
+// let b = 8;
+// // let b;
+// // b = 18;
+// // let b = 20;
+
+// console.log(first)
+
+
+
+// function add () {
+
+// }
+// debugger
+
+// const add = () => {
+
+// }
+
+
+
+// var a = 10;
+// a = 17;
+// var a;
+
+// let a = 12;
+//  a = 13;
+
+//  const a = 12;
+//   a = 13;
+//   console.log(a)
+
+let s;
+
+
+body
+
+
+
+function something() {
+    ///
+    ////
+    //
+    let data = '';
+    // body 
+    function first() {
+        data
+        ////
+        ////
+        ////
+        ///
+        // body 
+        function second() {
+            data
+            let body = null
+            ///
+            //
+            s = 'Reshab'
+            
+        }
+
+    }
+
+}
+console.log(s)
+
+
+
+function something2 () {
+
+    let b = s;
+
+}
+
