b5bea225526e1d7d0bfa2c4320460f75108855aa
Reshab Kumar <rkmodi12051998@gmail.com>
2026-01-04 16:28:37 +0530

    04-01-2026

--- index.js
+++ index.js
@@ -1,77 +1,130 @@
-// var a = 9;
-// // var a ;
-// // a = 16;
-// // var a = 18;
-// let b = 8;
-// // let b;
-// // b = 18;
-// // let b = 20;
+// // var a = 9;
+// // // var a ;
+// // // a = 16;
+// // // var a = 18;
+// // let b = 8;
+// // // let b;
+// // // b = 18;
+// // // let b = 20;
 
-// console.log(first)
-
-
-
-// function add () {
-
-// }
-// debugger
-
-// const add = () => {
-
-// }
+// // console.log(first)
 
 
 
-// var a = 10;
-// a = 17;
-// var a;
+// // function add () {
 
-// let a = 12;
-//  a = 13;
+// // }
+// // debugger
 
-//  const a = 12;
-//   a = 13;
-//   console.log(a)
+// // const add = () => {
 
-let s;
-
-
-body
+// // }
 
 
 
-function something() {
-    ///
-    ////
-    //
-    let data = '';
-    // body 
-    function first() {
-        data
-        ////
-        ////
-        ////
-        ///
-        // body 
-        function second() {
-            data
-            let body = null
-            ///
-            //
-            s = 'Reshab'
+// // var a = 10;
+// // a = 17;
+// // var a;
+
+// // let a = 12;
+// //  a = 13;
+
+// //  const a = 12;
+// //   a = 13;
+// //   console.log(a)
+
+// let s;
+
+
+// body
+
+
+
+// function something() {
+//     ///
+//     ////
+//     //
+//     let data = '';
+//     // body 
+//     function first() {
+//         data
+//         ////
+//         ////
+//         ////
+//         ///
+//         // body 
+//         function second() {
+//             data
+//             let body = null
+//             ///
+//             //
+//             s = 'Reshab'
             
-        }
+//         }
 
-    }
+//     }
 
+// }
+// console.log(s)
+
+
+
+// function something2 () {
+
+//     let b = s;
+
+// }
+
+
+
+// const x = prompt("Enter first value : ");
+// const y = prompt("Enter Second value : ");
+// const z = x + y
+// console.log(z)
+
+
+// const x = 10;
+// const y = '10';
+
+// // if(x == y)
+// if(x === y) {
+//     console.log('Equal')
+// } else {
+//     console.log('NE')
+// }
+
+
+
+
+
+// const x = '10'
+// const y = 10
+// console.log(x + y)
+
+// const data = parseFloat(prompt("Enter the Time: "))
+// const fname = prompt("Enter Your Name: ")
+
+// if(  data < 12 && data >= 6) {
+//     console.log(`Good morning ${fname}`)
+// } else if(data >= 12 && data < 16) {
+//         console.log(`Good Afternoon ${fname}`)
+// } else if(data >= 16 && data < 20) {
+//         console.log(`Good Evening ${fname}`)
+// } else if((data >= 20 && data < 24) || (data >= 0 && data < 6)) {
+//         console.log(`Good Night ${fname}`)
+// } else {
+//     console.log(`Enter the time between 0 to 24`)
+// }
+
+// 6 <= data < 12
+
+
+const arr = [1,2,3,4,5]
+
+for(let i= 0 ; i < arr.length; i++) {
+    console.log(arr[i]) 
 }
-console.log(s)
 
 
 
-function something2 () {
-
-    let b = s;
-
-}
 
