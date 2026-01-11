5d6784abb2d8305d45908fda9fcafc5b771e9cd4
Reshab Kumar <rkmodi12051998@gmail.com>
2026-01-11 15:17:57 +0530

    Object done

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-// // var a = 9;
+// add()
+
+
+ // var a = 9;
 // // // var a ;
 // // // a = 16;
 // // // var a = 18;
@@ -119,11 +122,230 @@
 // 6 <= data < 12
 
 
-const arr = [1,2,3,4,5]
+// const arr = [1,2,3,4,5]
 
-for(let i= 0 ; i < arr.length; i++) {
-    console.log(arr[i]) 
-}
+// for(let i= 0 ; i < arr.length; i++) {
+//     console.log(arr[i]) 
+// }
+
+
+// Operators :- Ternary, Null-col
+// Object Complete
+
+
+// const age = parseInt(prompt('Enter the age: '))
+
+// const msg = (age > 18) ? "Adult" : "Minor" ;
+// console.log(msg)
+
+// var ,let, const 
+
+// function doSomething () {
+//  console.log('Hello')
+// }
+
+
+// compareRank()
+// var inp = parseInt(prompt('Enter Rank'))
+
+// function compareRank () {
+//     const result = (inp < 100) ? true : false
+//     return result
+     
+// }
+// const msg = compareRank() ? 'Apply for next step' : "Better Luck Next time"
+
+// console.log(msg)
+
+
+
+
+
+// const text1 = undefined;
+// const text = null;
+// const data = text1 ?? "Missing";
+// console.log(data)
+
+// if(data > 18) {
+//     console.log("Adult")
+// } else {
+//     console.log("Minor")
+// }
+
+
+
+
+// function add()  {
+//     var a = 10;
+//     console.log('Hello')
+// }
+
+// add()
+// const add = () => {
+// console.log('Hello')
+// }
+
+// 2 () functions 
+
+// return 'Hello World'
+
+// add()
+
+// const msg = () => {
+//     console.log('Hello World') 
+// }
+// msg()
+
+// const inp1 = parseInt(prompt('Enter first Number: '))
+// const inp2 = parseInt(prompt('Enter 2nd Number: '))
+
+// const add = () => {
+//     console.log(inp1 + inp2)
+// }
+
+// add()
+
+// console.log(new Date().getDay())
+
+// switch(new Date().getDay()) {
+//     case 0:
+//         console.log('Sunday')
+//         break
+//     case 1:
+//         console.log('Monday')
+//         break
+//     case 2:
+//         console.log('Tuesday')
+//         break
+//     case 3:
+//         console.log('Wednesday')
+//         break
+//     case 4:
+//         console.log('Thursday')
+//         break
+//     case 5:
+//         console.log('Friday')  
+//         break 
+//     case 6:
+//         console.log('Saturday')
+    
+// }
+
+
+// Objects
+
+
+// const obj = {
+//     0: "Reshab",
+//     1: "kumar",
+//     age: 27
+// }
+// const arr = ["Reshab", "Kumar", 27]
+
+// console.log(typeof(obj))
+// console.log(typeof(arr))
+// let text = "";
+// const person = {
+    // first_name : "Reshab",
+    // last_name: "Kumar",
+    // age: 27,
+    // city: "Vaishali"
+    // full_data: function chat() {
+    //     text = this.first_name + " " + this.last_name
+    //     console.log(text)
+    // }
+// }
+// Object.seal(person)
+// person.state = "Bihar"
+// console.log(person)
+// person.full_data()
+
+
+
+// let hii = "reshab"
+
+// let  hii;
+// console.log(hii)
+
+// person.age = 28
+
+// console.log('person.age', person.age)
+
+// const person1 = new Object({
+//     first_name : "Reshab",
+//     last_name: "Kumar",
+//     age: 27,
+//     city: "Vaishali"
+// })
+// person1.state = "Bihar"
+
+// console.log(person1)
+
+// console.log(person1)
+
+
+// function Person(first, last, age) {
+//     this.first_name = last,
+//     this.last_name = first,
+//     this.age = age
+// }
+
+
+
+// const data = new Person("Reshab", "Kumar", 27)
+// const data1 = new Person("Ravi", "Kumar", 26)
+// const data2 = new Person("Baibhav", "Kumar", 27)
+// const data3 = new Person("Avinash", "Kumar", 27)
+// const data4 = new Person("Shubham", "Kumar", 29)
+
+// console.log(data)
+
+
+// function Bhopal(culture, place ) {
+//     this.culture_of_bhopal = culture,
+//     this.best_place_in_bhopal = place
+// }
+
+//    const data = new Bhopal("peaceful people", "Van Vihar")
+//    const data1 = new Bhopal("loving people", "PanchMani")
+
+//    console.log(data)
+//    console.log(data1)
+
+//    function 
+//    = () => {}
+//    ()
+
+
+const data = [1,2,3,4]
+const data1 = {a:1, b:2, c:3, d:4}
+data1[c]
+
+
+const key = Object.keys(data1)
+const values = Object.values(data1)
+const fdata = Object.entries(data1)
+console.log(key)
+console.log(values)
+console.log(fdata)
+
+
+// for(let x in data) {
+//     console.log(x)
+// }
+
+// for(let x of data) {
+//     console.log(x)
+// }
+
+
+
+
+
+
+
+
+
 
 
 
